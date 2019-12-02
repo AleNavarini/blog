80cfd7f3d9b198f4b28c3f7ca1d33b76bf9f5dad
Alejandro Navarini <ale.navarini99@gmail.com>
2019-12-01 23:36:25 -0300

    Post model hecho, con tinyMce

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+
+   
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length = 200)
+    subtitulo = models.CharField(max_length = 200)
+    autor = models.CharField(max_length = 200)
+    contenido = models.TextField()
+    fecha = models.DateTimeField("Fecha de Publicacion", default = datetime.now() )
+
+    def __str__(self):
+        return self.titulo
