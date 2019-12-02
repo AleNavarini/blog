39ac86a353f8fb8afb143d6d32bde77e1309f115
Alejandro Navarini <ale.navarini99@gmail.com>
2019-12-01 23:48:58 -0300

    Empezando la logica de home.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
+from blog.models import Post
 
 def homepage(request):
+
     return render(request,
-                  "blog/home.html")
+                  "blog/home.html",
+                  {"posts": Post.objects.all})
