fb839a33ff14786399e395ea34831f42c078d0c8
Alejandro Navarini <ale.navarini99@gmail.com>
2019-12-12 00:09:59 -0300

    12/12/19 12.09pm

--- blog/urls.py
+++ blog/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 app_name = "blog"
 
 urlpatterns = [
     path("", views.homepage, name = "homepage"),
+    re_path("bio/", views.bio, name = "bio"),
+    re_path("colaboraciones/", views.colaboraciones, name = "colab"),
+    re_path("articulos/", views.articulos, name = "articulos"),
+    re_path('busca/$',views.buscaArticulos, name = 'buscaArticulos'),
+
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,27 @@ def homepage(request):
     return render(request,
                   "blog/home.html",
                   {"posts": Post.objects.all})
+
+def bio(request):
+
+    return render(request,
+                  "blog/bio.html")
+
+def colaboraciones(request):
+
+    return render(request,
+                  "blog/colab.html",
+                  )
+
+def articulos(request):
+
+    return render(request,
+                  "blog/home.html",
+                  {"posts": Post.objects.all})
+
+def buscaArticulos(request):
+    
+    search = request.GET.get('search')
+    post = Post.objects.get(pk = postid)
+	
+    return render(request, "main/buscaArticulos.html",context = {"post": post})
