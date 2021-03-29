e620954b6b7c1e698dbcf9eb28ee328946e7ee7a
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2021-03-29 00:04:39 -0300

    Page refactor, changed design

---
+++ blog/static/logic.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function hover(element) {
+    element.setAttribute('src', "static/images/logoFin.png");
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', "static/images/logoIni.png");
+  }
